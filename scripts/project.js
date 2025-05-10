99f5f0ca1b1c250d441638f68afc460e40d8ac28
Josué Benjamín Pacheco <josue2.benjamin6@gmail.com>
2025-05-10 14:53:27 -0600

    Project changes

--- scripts/project.js
+++ scripts/project.js
@@ -6,36 +6,30 @@ demoImages.forEach(image => {
             resetImage(image);
         } else {
             demoImages.forEach(img => resetImage(img));
-
             activateImage(image);
         }
     });
 });
 
 function activateImage(image) {
-    // image.style.backgroundColor = "rgb(0, 0, 0)";
-    image.style.backgroundColor = "rgb(7, 18, 60)";
+    image.classList.add('active');
     image.style.display = "flex";
     image.style.justifyContent = "center";
     image.style.alignItems = "center";
-    image.style.cursor = "default";
-    image.innerHTML = `
-        <a href="#">
+    image.innerHTML += `
+        <a href="https://github.com/JosuePachecoDev/password-generator.git">
             <img class="project-svg" src="multimedia_files/github-isotype.svg" alt="">
         </a>
-        <a href="#">
+        <a href="https://josuepachecodev.github.io/password-generator/" target="_blank">
             <img class="project-svg" src="multimedia_files/new-tab-icon.svg" alt="">
         </a>
     `;
-    image.classList.add('active');
 }
 
 function resetImage(image) {
-    image.style.backgroundColor = "rgb(7, 18, 136)";
+    image.classList.remove('active');
     image.style.display = "";
     image.style.justifyContent = "";
     image.style.alignItems = "";
-    image.style.cursor = "pointer";
-    image.innerHTML = "";
-    image.classList.remove('active');
+    image.innerHTML = '<div class="overlay"></div>';
 }
