fea824ea5915188dff9eb092eeb4928e9d9ff87b
Josué Benjamín Pacheco <josue2.benjamin6@gmail.com>
2025-05-14 15:42:16 -0600

    Hover using the viewport

--- scripts/mouse-detection.js
+++ scripts/mouse-detection.js
@@ -2,21 +2,11 @@ const projectBox = document.querySelectorAll(".project");
 const projectImage = document.querySelectorAll(".pi");
 let theresMouse = false;
 
-window.addEventListener("mousemove", mouseDetected);
-
-function mouseDetected() {
-    theresMouse = true;
-    window.removeEventListener("mousemove", mouseDetected);
-    hoverWithMouse();
-}
-
-setTimeout(() => {
-    if (!theresMouse) {
-        console.log("📱 No se detectó cursor. Posible pantalla táctil.");
-        clearStyles();
-        intersectionObserver();
-    }
-}, 2000);
+if (window.matchMedia("(max-width: 768px)").matches) {
+  intersectionObserver();
+} else {
+  hoverWithMouse();
+};
 
 function hoverWithMouse() {
     projectBox.forEach(div => {
@@ -37,10 +27,7 @@ function hoverWithMouse() {
         div.addEventListener("mouseenter", () => {
             div.classList.add("pi-on");
             div.classList.add("active");
-            div.style.display = "flex";
-            div.style.justifyContent = "center";
-            div.style.alignItems = "center";
-            div.innerHTML += `
+            div.innerHTML = `
                 <a href="https://github.com/JosuePachecoDev/password-generator.git">
                     <img class="project-svg" src="multimedia_files/github-isotype.svg" alt="">
                 </a>
@@ -55,9 +42,6 @@ function hoverWithMouse() {
         div.addEventListener("mouseleave", () => {
             div.classList.remove("pi-on");
             div.classList.remove("active");
-            div.style.display = "";
-            div.style.justifyContent = "";
-            div.style.alignItems = "";
             div.innerHTML = "";
         });
     });
@@ -66,15 +50,28 @@ function hoverWithMouse() {
 function intersectionObserver() {    
     const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            
-        } else {
-            entry.target.classList.remove('visible');
-        }
+        const project = entry.target;
+        const pi = project.querySelector('.pi');
+
+            if (entry.isIntersecting) {
+                project.classList.add('visible');
+                pi.classList.add('pi-on');
+                pi.innerHTML = `
+                <a href="https://github.com/JosuePachecoDev/password-generator.git">
+                    <img class="project-svg" src="multimedia_files/github-isotype.svg" alt="">
+                </a>
+                <a href="https://josuepachecodev.github.io/password-generator/" target="_blank">
+                    <img class="project-svg" src="multimedia_files/new-tab-icon.svg" alt="">
+                </a>
+            `;
+            } else {
+                project.classList.remove('visible');
+                pi.classList.remove('pi-on');
+                pi.innerHTML = "";
+            }
     });
     }, {
-    threshold: 1
+    threshold: 0.85
     });
 
     projectBox.forEach(proj => observer.observe(proj));
