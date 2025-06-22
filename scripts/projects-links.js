bdb94e518b10eb48ff115a762d796e34b4fb6360
Josué Benjamín Pacheco <josue2.benjamin6@gmail.com>
2025-06-22 15:37:06 -0600

    Responsive design bug from section "about me" solved

--- scripts/projects-links.js
+++ scripts/projects-links.js
@@ -1,16 +1,9 @@
 const projectBox = document.querySelectorAll(".project");
 const projectImage = document.querySelectorAll(".pi");
-const piOne = document.getElementById("project-1");
-const piTwo = document.getElementById("project-2");
-const piThree = document.getElementById("project-3");
-const piFour = document.getElementById("project-4");
-const piFive = document.getElementById("project-5");
-const piSix = document.getElementById("project-6");
 
 if (window.matchMedia("(max-width: 768px)").matches) {
     intersectionObserver();
 } else {
-    projectsTab();
     hoverWithMouse();
     iconsHover();
 };
@@ -142,67 +135,3 @@ function intersectionObserver() {
 
     projectBox.forEach(proj => observer.observe(proj));
 };
-
-function projectsTab() {
-    projectBox.forEach(div => {
-        div.addEventListener("focus", () => {
-            div.style.transform = "scale(1.02)";
-            if (div.id === "p1") {
-                piOne.innerHTML = `
-                    <a href="https://github.com/JosuePachecoDev/password-generator.git">
-                        <img class="project-svg" src="media/icons/github-isotype.svg" alt="">
-                    </a>
-                    <a href="https://josuepachecodev.github.io/password-generator/" target="_blank" rel="noopener noreferrer">
-                        <img class="project-svg" src="#" alt="">
-                    </a>
-                `;
-                piOne.classList.add("pi-on");
-                piOne.classList.add("active");
-                clearFocusStyle(piTwo, piThree);
-            } else if (div.id === "p2") {
-                div.style.transform = "scale(1.02)";
-                piTwo.innerHTML = `
-                <a href="https://github.com/JosuePachecoDev/page-not-found.git">
-                <img class="project-svg" src="media/icons/github-isotype.svg" alt="">
-                </a>
-                <a href="https://josuepachecodev.github.io/page-not-found/" target="_blank" rel="noopener noreferrer">
-                <img class="project-svg" src="#" alt="">
-                </a>
-                `;
-                piTwo.classList.add("pi-on");
-                piTwo.classList.add("active");
-                clearFocusStyle(piOne, piThree);
-            } else if (div.id === "p3") {
-                div.style.transform = "scale(1.02)";
-                piThree.innerHTML = `
-                    <a href="https://github.com/JosuePachecoDev/coffe-shop-website.git">
-                        <img class="project-svg" src="media/icons/github-isotype.svg" alt="">
-                    </a>
-                    <a href="https://josuepachecodev.github.io/coffe-shop-website/" target="_blank" rel="noopener noreferrer">
-                        <img class="project-svg" src="#" alt="">
-                    </a>
-                `;
-                piThree.classList.add("pi-on");
-                piThree.classList.add("active");
-                clearFocusStyle(piOne, piTwo);
-            }
-        });
-
-        div.addEventListener("blur", () => {
-            div.style.transform = "scale(1)";
-        })
-
-        /* if (document.activeElement == div) {
-            alert(`El elemento ${div.id} está enfocado.`);
-        } */
-    });
-};
-
-function clearFocusStyle(elementOne, elementTwo) {
-    elementOne.innerHTML = "";
-    elementOne.classList.remove("pi-on");
-    elementOne.classList.remove("active");
-    elementTwo.innerHTML = "";
-    elementTwo.classList.remove("pi-on");
-    elementTwo.classList.remove("active");
-}
