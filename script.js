1218441903f78f2483a1840d6e0855240e4b238d
Josué Benjamín Pacheco <josue2.benjamin6@gmail.com>
2025-07-14 14:01:14 -0600

    Typewriter effect width error fixed on phones viewport

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const projectBox = document.querySelectorAll(".project");
 const projectImage = document.querySelectorAll(".pi");
-const projectOneImageIcon = document.querySelectorAll(".pii-1")
-const projectTwoImageIcon = document.querySelectorAll(".pii-2")
-const projectThreeImageIcon = document.querySelectorAll(".pii-3")
+const projectOneImageIcon = document.querySelectorAll(".pii-1");
+const projectTwoImageIcon = document.querySelectorAll(".pii-2");
+const projectThreeImageIcon = document.querySelectorAll(".pii-3");
+const textTypewriter = document.getElementById("name-am");
 
 if (window.matchMedia("(max-width: 768px)").matches) {
     intersectionObserver();
+    if (window.matchMedia("(max-width: 390px)").matches) {
+        textTypewriter.classList.remove("typewriter");
+    }
 } else {
     projectsHoverWithMouse();
     iconsHover();
 }
+
+projectBox.forEach(project => {
+    const img = project.querySelector(".pi");
+    const imgIcos = img.querySelectorAll("a");
+    project.addEventListener("focus", () => {
+        img.classList.add("pi-on");
+        imgIcos.forEach(icon => {
+            icon.style.display = "initial";
+            icon.style.opacity = "100%";
+        });
+    });
+
+    project.addEventListener("blur", () => {
+        img.classList.remove("pi-on");
+        imgIcos.forEach(icon => {
+            icon.style.display = "none";
+            icon.style.opacity = "0%";
+        });
+    });
+});
 
 function projectsHoverWithMouse() {
     projectBox.forEach(div => {
@@ -97,22 +121,32 @@ function intersectionObserver() {
     projectBox.forEach(proj => observer.observe(proj));
 }
 
-projectBox.forEach(project => {
-    const img = project.querySelector(".pi");
-    const imgIcos = img.querySelectorAll("a");
-    project.addEventListener("focus", () => {
-        img.classList.add("pi-on");
-        imgIcos.forEach(icon => {
-            icon.style.display = "initial";
-            icon.style.opacity = "100%";
+function iconsHover() {
+    const allIcons = document.querySelectorAll('.smi');
+    
+    allIcons.forEach(svg => {
+        svg.addEventListener("mouseenter", (e) => {
+            if (e.target.id === "behance-as") {
+                e.target.classList.add("behance-fill");
+            } else if ((e.target.id === "linkedin-as") ||(e.target.id === "linkedin-sm")) {
+                e.target.classList.add("linkedin-fill");
+            } else if (e.target.id === "instagram-sm") {
+                e.target.classList.add("instagram-fill");
+            } else if (e.target.id === "facebook-sm") {
+                e.target.classList.add("facebook-fill");
+            } else if (e.target.id === "whatsapp-sm") {
+                e.target.classList.add("whatsapp-fill");
+            }
         });
     });
 
-    project.addEventListener("blur", () => {
-        img.classList.remove("pi-on");
-        imgIcos.forEach(icon => {
-            icon.style.display = "none";
-            icon.style.opacity = "0%";
-        });
+    allIcons.forEach(svg => {
+        svg.addEventListener("mouseleave", () => {
+            svg.classList.remove("linkedin-fill");
+            svg.classList.remove("behance-fill");
+            svg.classList.remove("instagram-fill");
+            svg.classList.remove("facebook-fill");
+            svg.classList.remove("whatsapp-fill");
+        })
     });
-});
+}
